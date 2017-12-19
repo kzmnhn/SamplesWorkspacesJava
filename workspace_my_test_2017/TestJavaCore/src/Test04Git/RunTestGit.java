674887264b641b722ccdd61b96449358c775a46e
Natalia K. <kzmnhn@gmail.com>
2017-12-19 20:46:39 +0300

    recommit this libs, which was outside

--- workspace_my_test_2017/TestJavaCore/src/Test04Git/RunTestGit.java
+++ workspace_my_test_2017/TestJavaCore/src/Test04Git/RunTestGit.java
@@ -5,7 +5,7 @@ public class RunTestGit {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		
-		System.out.println("commit through eclipse");
+		System.out.println("re'commit through eclipse");
 	}
 
 }
